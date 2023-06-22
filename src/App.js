6ee45f06a04ad453dd9131ce9dd38be78df3f45c
Luciano Moyano <lucianoarielmoyano@outlook.com>
2023-06-21 21:47:15 -0300

    contador con redux

--- src/App.js
+++ src/App.js
@@ -1,48 +1,63 @@
-import { legacy_createStore as createStore } from "redux"; //Lugar donde se centraliza todo el estado de la aplicación
-import { Provider, connect } from "react-redux"; //bibliotecas para utilizar redux
-
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
 import "./App.css";
 
-// Definir el estado inicial
-
+// Define the initial state
 const initialState = {
   count: 0,
 };
 
-//Definir un reducer que actualice el estado
-
+// Define a reducer that updates the state
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "INCREMENT":
-      return { count: (state.count = +1) };
+      return { count: state.count + 1 };
     case "DECREMENT":
-      return { count: (state.count = -1) };
-  } // la accion global que utiliza nuestro componente
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
 };
 
-// crear el store de Redux
-
+// Create the Redux store
 const store = createStore(reducer);
 
-const Counter = () => {
+const Counter = ({ count, increment, decrement }) => {
   return (
     <>
       <h3>
-        Contador: <span>0</span>
+        Contador: <span>{count}</span>
       </h3>
-      <button>Incrementar</button>
-      <button>Decrementar</button>
+      <button onClick={increment}>Incrementar</button>
+      <button onClick={decrement}>Decrementar</button>
     </>
   );
 };
 
+// Map the state and actions to the component
+const mapStateToProps = (state) => {
+  return {
+    count: state.count,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch({ type: "INCREMENT" }),
+    decrement: () => dispatch({ type: "DECREMENT" }),
+  };
+};
+
+// Connect the component to the Redux store
+const ConnectedCounter = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+// Main component
 function App() {
   return (
     <>
-      <div className="App">
-        <h1>redux</h1>
-        <Counter />
-      </div>
+      <Provider store={store}>
+        <ConnectedCounter />
+      </Provider>
     </>
   );
 }
